b0195a6b3368df382ef2aad9718090c8444f8720
Zarone <redninjadolphin1@hotmail.com>
2021-01-07 19:04:25 -0500

    first commit

---
+++ editor/events.js
@@ -0,0 +1,146 @@
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  imageDiv.innerHTML = '';
+  loading.hidden = false;
+  let images;
+  try {
+    images = await getImages(searchBox.value);
+    if (images.length > 0){
+      let loadCounter = Math.min(images.length, 100);
+      for (let i = 0; i < loadCounter; i++){
+        let img = document.createElement('IMG');
+        img.src = images[i]['URL'];
+        img.alt = images[i]['Name']
+        img.classList.add('cardImage')
+        img.addEventListener('click', ()=>{
+          newCard = false;
+          editCard(img.src, img.alt);
+        })
+        imageDiv.appendChild(img);
+      }
+    } else {
+      let p = document.createElement('P');
+      p.innerText = 'No Search Results Found';
+      imageDiv.appendChild(p);
+    }
+  } catch (err){
+    let p = document.createElement('P');
+    p.innerText = err;
+    imageDiv.appendChild(p);
+  }
+  
+  loading.hidden = true;
+
+})
+
+saveEdit.addEventListener('click', async ()=>{
+  imageDiv.innerHTML = '';
+  loading.hidden = false;
+  if (newCard == false){
+    let newObject = {
+      Name: dataPoints.name.value,
+      Type: dataPoints.type.value,
+      SubType: dataPoints.subtype.value,
+      Cost: dataPoints.cost.value,
+      Power: dataPoints.power.value,
+      PowerType: dataPoints.powerType.value,
+      Level: dataPoints.level.value,
+      EffectType: dataPoints.effectType.value,
+      Effect: dataPoints.cardEffect.value,
+      URL: dataPoints.url.value
+    }
+
+    
+    await fetch(fetchURL, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newObject)
+    })
+
+    try {
+      images = await getImages(searchBox.value);
+      if (images.length > 0){
+        let loadCounter = Math.min(images.length, 100);
+        for (let i = 0; i < loadCounter; i++){
+          let img = document.createElement('IMG');
+          img.src = images[i]['URL'];
+          img.alt = images[i]['Name']
+          img.classList.add('cardImage')
+          img.addEventListener('click', ()=>{
+            editCard(img.src, img.alt);
+          })
+          imageDiv.appendChild(img);
+        }
+      } else {
+        let p = document.createElement('P');
+        p.innerText = 'No Search Results Found';
+        imageDiv.appendChild(p);
+      }
+    } catch (err){
+      let p = document.createElement('P');
+      p.innerText = err;
+      imageDiv.appendChild(p);
+    }
+  } else { // if newCard is true, this section uses POST
+    let response;
+    try{
+      response = await fetch(fetchURL, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          Name: dataPoints["name"].value.toString(),
+          Type: dataPoints["type"].value.toString(),
+          SubType: dataPoints["subtype"].value.toString(),
+          Cost: dataPoints["cost"].value.toString(),
+          Power: dataPoints["power"].value.toString(),
+          PowerType: dataPoints["powerType"].value.toString(),
+          Level: dataPoints["level"].value.toString(),
+          EffectType: dataPoints["effectType"].value.toString(),
+          Effect: dataPoints["cardEffect"].value.toString(),
+          URL: dataPoints["url"].value.toString()
+        })
+      })    
+      response = await response.json();
+      console.log(response)
+      if (response == false){
+        let p = document.createElement('P');
+        p.innerText = 'That card already exists';
+        imageDiv.appendChild(p);
+      }
+    
+    } catch (err){
+      console.error(err);
+    }
+
+  }
+
+  loading.hidden = true;
+  hide()
+
+})
+
+createCard.addEventListener('click', async(e)=>{
+  e.preventDefault();
+  show();
+  selectedCardImage.src = "c:/Users/Zachary%20Alfano/Desktop/Games/Guardians/background.jpg"
+  newCard = true;
+  
+  dataPoints.name.value = '';
+  dataPoints.type.value = '';
+  dataPoints.subtype.value = '';
+  dataPoints.cost.value = 0;
+  dataPoints.power.value = 0;
+  dataPoints.powerType.value = '';
+  dataPoints.level.value = 0;
+  dataPoints.effectType.value = '';
+  dataPoints.cardEffect.value = '';
+  dataPoints.url.value = '';
+
+})
+
---
+++ editor/main.js
@@ -0,0 +1,108 @@
+const options = JSON.stringify({
+  Type: 'Character', 
+  // possible: Action, Event, Character
+  SubType: 'Special Character', 
+  //possible: Action: Location - Unity, Location, Perpetual - Unity, Perpetual, Action - Unity, Action
+  //possible: Event: Normal, Climax
+  //possible: Character: Special Character - Unleashed, Special Character, Basic Character
+  Cost: 10,
+  Effect: '',
+  EffectType: 'Activate',
+  Level: 3,
+  Name: 'The Architect',
+  URL: 'C:/Users/Zachary Alfano/Desktop/Games/Guardians/Cards/The Architect Level 3-01.jpg',
+  Power: "8",
+  PowerType: "Aether"
+})
+
+const form = document.getElementById('searchContainer');
+const searchBox = document.getElementById('searchBox');
+const loading = document.getElementById('loading');
+const imageDiv = document.getElementById('imageDiv');
+const editBack = document.getElementById('editBack');
+const editMenu = document.getElementById('editMenu');
+const selectedCardImage = document.getElementById('selectedCardImage');
+const saveEdit = document.getElementById('saveEdit');
+const createCard = document.getElementById('createCard')
+
+const dataPoints = {
+  name: document.getElementById('simpleMenu0'),
+  type: document.getElementById('simpleMenu1'),
+  subtype: document.getElementById('simpleMenu2'),
+  cost: document.getElementById('simpleMenu3'),
+  power: document.getElementById('simpleMenu4'),
+  powerType: document.getElementById('simpleMenu5'),
+  level: document.getElementById('simpleMenu6'),
+  effectType: document.getElementById('effectType'),
+  cardEffect: document.getElementById('effectText'),
+  url: document.getElementById('urlSelect')
+}
+
+let newCard;
+// const fetchURL = 'http://localhost:3000/'
+const fetchURL = 'https://fast-scrubland-61023.herokuapp.com/'
+
+function hide(){
+  editBack.hidden = true;
+  editMenu.hidden = true;
+}
+
+function show(){
+  editBack.hidden = false;
+  editMenu.hidden = false;
+}
+
+async function getImages(target) {
+  let cards;
+  if (target == ''){
+    cards = await fetch(fetchURL, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  } else if (target.toString().indexOf('/') == -1){
+    cards = await fetch(fetchURL+target.toString(), {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+  
+  return cards.json();
+}
+
+async function editCard(id, name) {
+  editBack.hidden = false;
+  editMenu.hidden = false;
+  selectedCardImage.src = id;
+  
+  let cardInfo;
+  try {
+    cardInfo = await fetch(fetchURL + "/Name/"+name, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+  cardInfo = await cardInfo.json();
+
+  dataPoints.name.value = cardInfo[0]['Name'];
+  dataPoints.type.value = cardInfo[0]['Type'];
+  dataPoints.subtype.value = cardInfo[0]['SubType'];
+  dataPoints.cost.value = cardInfo[0]['Cost'];
+  dataPoints.power.value = cardInfo[0]['Power'];
+  dataPoints.powerType.value = cardInfo[0]['PowerType'];
+  dataPoints.level.value = cardInfo[0]['Level'];
+  dataPoints.effectType.value = cardInfo[0]['EffectType'];
+  dataPoints.cardEffect.value = cardInfo[0]['Effect'];
+  dataPoints.url.value = cardInfo[0]['URL'];
+
+}
